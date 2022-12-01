fec2960a5c4f9ea8176afd2ffb93ec55882a9738
Pedro Viana <pedroviana.developer13@gmail.com>
2022-12-01 16:42:46 -0300

    requisito 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Form from './components/Form';
 
-state = {
-  cardName: '',
-  cardDescription: '',
-  cardAttr1: '',
-  cardAttr2: '',
-  cardAttr3: '',
-  cardImage: '',
-  cardRare: '',
-  cardTrunfo: false,
-  hasTrunfo: false,
-  isSaveButtonDisabled: false,
-  onInputChange,
-  onSaveButtonClick,
-};
-
 class App extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.onInputChange = this.onInputChange.bind(this);
+  //   this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+  //   this.state = {
+  //     cardName: '',
+  //     cardDescription: '',
+  //     cardAttr1: '',
+  //     cardAttr2: '',
+  //     cardAttr3: '',
+  //     cardImage: '',
+  //     cardRare: '',
+  //     cardTrunfo: false,
+  //     hasTrunfo: false,
+  //     isSaveButtonDisabled: false,
+  //     onInputChange,
+  //     onSaveButtonClick,
+  //   };
+  // }
+
+  // onInputChange() {}
+
+  // onSaveButtonClick() {}
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
 
-        <Form { ...this.state } />
+        <Form />
       </div>
     );
   }
