421e290e5379dfd87b64a1eef440188594b26481
Pedro Viana <pedroviana.developer13@gmail.com>
2022-12-01 15:00:57 -0300

    lint requisito 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,31 +4,31 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <label>
+        <label htmlFor="text">
           Nome da carta:
           <input type="text" data-testid="name-input" />
         </label>
-        <label>
+        <label htmlFor='textarea'>
           Descrição da carta:
           <input type="textarea" data-testid="description-input" />
         </label>
-        <label>
+        <label htmlFor='number'>
           Atributo 1:
           <input type="number" data-testid="attr1-input" />
         </label>
-        <label>
+        <label htmlFor='number'>
           Atributo 2:
           <input type="number" data-testid="attr2-input" />
         </label>
-        <label>
+        <label htmlFor='number'>
           Atributo 3:
           <input type="number" data-testid="attr3-input" />
         </label>
-        <label>
+        <label htmlFor='text'>
           Caminho da imagem da carta:
           <input type="text" data-testid="image-input" />
         </label>
-        <label>
+        <label htmlFor='select'>
           Raridade da carta:
           <select type="select" data-testid="rare-input">
             <option>normal</option>
@@ -36,11 +36,11 @@ class Form extends React.Component {
             <option>muito raro</option>
           </select>
         </label>
-        <label>
+        <label htmlFor='checkbox'>
           Esta carta é super trunfo?
           <input type="checkbox" data-testid="trunfo-input" />
         </label>
-        <button data-testid="save-button">Salvar</button>
+        <button type="button" data-testid="save-button">Salvar</button>
       </form>
     );
   }
