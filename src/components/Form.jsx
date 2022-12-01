f9515de124d873314c7b47c0bc98ba85244c2685
Pedro Viana <pedroviana.developer13@gmail.com>
2022-12-01 14:57:16 -0300

    iniciando o projeto e acredito que o requisito 1 ja foi

---
+++ src/components/Form.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>
+          Nome da carta:
+          <input type="text" data-testid="name-input" />
+        </label>
+        <label>
+          Descrição da carta:
+          <input type="textarea" data-testid="description-input" />
+        </label>
+        <label>
+          Atributo 1:
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label>
+          Atributo 2:
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label>
+          Atributo 3:
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label>
+          Caminho da imagem da carta:
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label>
+          Raridade da carta:
+          <select type="select" data-testid="rare-input">
+            <option>normal</option>
+            <option>raro</option>
+            <option>muito raro</option>
+          </select>
+        </label>
+        <label>
+          Esta carta é super trunfo?
+          <input type="checkbox" data-testid="trunfo-input" />
+        </label>
+        <button data-testid="save-button">Salvar</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
