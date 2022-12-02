1b92c42d7d506aacee7fcface6effbf478aacff1
Pedro Viana <pedroviana.developer13@gmail.com>
2022-12-01 21:26:21 -0300

    requisito 5 mas acho que o lint vai chorar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.onInputChange = this.onInputChange.bind(this);
-    this.state = {
+    const INITIAL_STATE = {
       cardName: '',
       cardDescription: '',
       cardAttr1: 0,
@@ -16,7 +16,11 @@ class App extends React.Component {
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
+    };
+
+    this.state = {
+      ...INITIAL_STATE,
     };
   }
 
@@ -25,6 +29,43 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, this.validateInputs);
+  };
+
+  validateInputs = () => {
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare } = this.state;
+
+    const sumAttr = 210;
+    const maxAttr = 90;
+    const minAttr = 0;
+
+    const attrValidate = Number(cardAttr1) <= maxAttr && Number(cardAttr1) >= minAttr
+      && Number(cardAttr2) <= maxAttr && Number(cardAttr2) >= minAttr
+      && Number(cardAttr3) <= maxAttr && Number(cardAttr3) >= minAttr;
+
+    const sumValidate = Number(cardAttr1)
+     + Number(cardAttr2)
+      + Number(cardAttr3) <= sumAttr;
+
+    const nameValidate = cardName.length > 0;
+    const imgValidate = cardImage.length > 0;
+    const descriptionValidate = cardDescription.length > 0;
+    const rareValidate = cardRare.length > 0;
+
+    this.setState({
+      isSaveButtonDisabled: !(attrValidate
+          && sumValidate
+          && nameValidate
+          && imgValidate
+          && descriptionValidate
+          && rareValidate),
     });
   };
 
@@ -37,6 +78,7 @@ class App extends React.Component {
           { ...this.state }
           onInputChange={ this.onInputChange }
         />
+
         <Card
           { ...this.state }
           onInputChange={ this.onInputChange }
